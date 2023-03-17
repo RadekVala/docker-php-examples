6a4b01cd18c7f4aa707fee05fd0ba3a4ddec574e
Radek Vala <radek.www@gmail.com>
2023-03-17 13:04:49 +0100

    Use UserRepository in register.php form, for storing new user into DB composer install needs to be run in App container & composer dump-autoload

--- app/public/register.php
+++ app/public/register.php
@@ -1,4 +1,8 @@
 <?php
+    use App\Repository\UserRepository;
+    use App\Model\User;
+
+    require('../vendor/autoload.php');
 
     $username = trim($_POST['username']);
     $email = trim($_POST['email']);
@@ -16,12 +20,16 @@
         $error = "Password and its confirmation does not match";
     }
     else {
-
         // All validation passed, do something with the data
         // For example, save the user to a database
-        // And then redirect to a success page
-        header("Location: index.php?registration=success");
-        exit();
+
+        $user = new User($username, $email);
+        $userRepository = new UserRepository();
+        if ($userRepository->store($user)){
+            // And then redirect to a success page
+            header("Location: index.php?registration=success");
+            exit();
+        }
     }
 
     header("Location: index.php?error=$error");
